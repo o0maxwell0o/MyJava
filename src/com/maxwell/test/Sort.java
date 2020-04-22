5aa55d2fc79107bb0fe6f25a32aa77e07fd6460b
o0maxwell0o <40237850+o0maxwell0o@users.noreply.github.com>
2020-04-23 01:45:02 +0900

    heap sort

--- src/com/maxwell/test/Sort.java
+++ src/com/maxwell/test/Sort.java
@@ -7,8 +7,10 @@ import java.util.Map;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] a = {3, 11, 4, 3, 222, 12312, 2222, 6};
-        radixSort(a);
+        int[] a = {7, 200, 1000, 300, 3, 233, 13, 500};
+        heapSort_asc(a);
+        System.out.println(Arrays.toString(a));
+
     }
 
     public static void bubbleSort(int[] array) {
@@ -214,6 +216,107 @@ public class Sort {
             System.out.println(Arrays.toString(array));
         }
 
+    }
+
+    public static void bucketSort(int[] array) {
+        int length = array.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int bucketCount = 10;//默认设置4个桶
+        for (int i = 0; i < length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        int range = (max - min + 1) / bucketCount;//每个桶的范围
+        if (range != (double) (max - min + 1) / (double) bucketCount) {
+            range++;
+        }
+        ArrayList<ArrayList<Integer>> bucketArr = new ArrayList<>();
+        for (int i = 0; i < bucketCount; i++) {
+            bucketArr.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < length; i++) {
+            int val = array[i];
+            // 计算当前值属于哪个桶
+            int bucketIndex = (int) Math.floor((val - min) / range);
+            // 向桶中添加元素
+            ArrayList<Integer> arrayList = bucketArr.get(bucketIndex);
+            if (arrayList.isEmpty()) {
+                arrayList.add(array[i]);
+            } else {
+                boolean checkEnd = true;
+                for (int index = 0; index < arrayList.size(); index++) {
+                    if (array[i] < arrayList.get(index)) {
+                        arrayList.add(index, array[i]);
+                        checkEnd = false;
+                        break;
+                    }
+                }
+                if (checkEnd) {
+                    arrayList.add(array[i]);
+                }
+            }
+        }
+        int putIndex = 0;
+        for (ArrayList<Integer> arr : bucketArr) {
+            for (int i : arr) {
+                array[putIndex] = i;
+                putIndex++;
+            }
+            System.out.println(Arrays.toString(array));
+        }
+    }
+
+    public static void maxheap_down(int a[], int start, int end) {
+        int c = start;            // 当前(current)节点的位置
+        int l = 2 * c + 1;        // 左(left)孩子的位置
+        int tmp = a[c];            // 当前(current)节点的大小
+        for (; l <= end; c = l, l = 2 * l + 1) {
+            // "l"是左孩子，"l+1"是右孩子
+            if (l < end && a[l] < a[l + 1])
+                l++;        // 左右两孩子中选择较大者，即m_heap[l+1]
+            if (tmp >= a[l])
+                break;        // 调整结束
+            else            // 交换值
+            {
+                a[c] = a[l];
+                a[l] = tmp;
+            }
+        }
+    }
+
+    /*
+     * 堆排序(从小到大)
+     *
+     * 参数说明：
+     *     a -- 待排序的数组
+     *     n -- 数组的长度
+     */
+    public static void heapSort_asc(int a[]) {
+        int i;
+        int n = a.length;
+        // 从(n/2-1) --> 0逐次遍历。遍历之后，得到的数组实际上是一个(最大)二叉堆。
+        for (i = n / 2 - 1; i >= 0; i--)
+            maxheap_down(a, i, n - 1);
+
+        // 从最后一个元素开始对序列进行调整，不断的缩小调整的范围直到第一个元素
+        for (i = n - 1; i > 0; i--) {
+            // 交换a[0]和a[i]。交换后，a[i]是a[0...i]中最大的。
+            swap(a, 0, i);
+            // 调整a[0...i-1]，使得a[0...i-1]仍然是一个最大堆。
+            // 即，保证a[i-1]是a[0...i-1]中的最大值。
+            maxheap_down(a, 0, i - 1);
+        }
+    }
+
+    public static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 
 }
