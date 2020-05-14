89ebc124e8fe86c30cafc9e942880195c1a1510a
o0maxwell0o <40237850+o0maxwell0o@users.noreply.github.com>
2020-05-15 04:32:05 +0900

    datacode tree

---
+++ src/com/maxwell/test/leetcode/MinimumAbsoluteDifferenceBST_530.java
@@ -0,0 +1,63 @@
+package com.maxwell.test.leetcode;
+
+public class MinimumAbsoluteDifferenceBST_530 {
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+
+    int last;
+    int diff;
+
+    public int getMinimumDifference(TreeNode root) {
+        last = Integer.MAX_VALUE;
+        diff = Integer.MAX_VALUE;
+        find(root);
+        return diff;
+    }
+
+    public void find(TreeNode node) {
+        if (node == null)
+            return;
+        find(node.left);
+        diff = Math.min(diff, Math.abs(last - node.val));
+        last = node.val;
+        find(node.right);
+
+    }
+
+}
